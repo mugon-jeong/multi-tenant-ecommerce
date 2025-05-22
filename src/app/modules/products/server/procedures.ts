f882b808f5f1e906dcddaa3479e5894b2f690775
conny <conny@musma.net>
2025-05-22 09:08:55 +0900

    12 Filtering Products fix

--- src/app/modules/products/server/procedures.ts
+++ src/app/modules/products/server/procedures.ts
@@ -13,13 +13,19 @@ export const productsRouter = createTRPCRouter({
       })
     )
     .query(async ({ ctx, input }) => {
-      const where: Where = {}
-      if (input.minPrice) {
+      const where: Where = {
+        price: {},
+      }
+      if (input.minPrice && input.maxPrice) {
+        where.price = {
+          less_than_equal: input.maxPrice,
+          greater_than_equal: input.minPrice,
+        }
+      } else if (input.minPrice) {
         where.price = {
           greater_than_equal: input.minPrice,
         }
-      }
-      if (input.maxPrice) {
+      } else if (input.maxPrice) {
         where.price = {
           less_than_equal: input.maxPrice,
         }
